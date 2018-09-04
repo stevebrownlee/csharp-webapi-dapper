692e87bbb685a2bf53a8c76926e9781e1657d347
Steve Brownlee <chortlehoort@gmail.com>
2018-09-03 22:03:42 -0500

    Implemented PUT method

--- Controllers/ExerciseController.cs
+++ Controllers/ExerciseController.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using NSSWeb.Data;
 using Dapper;
+using Microsoft.AspNetCore.Http;
 
 namespace NSSWeb.Controllers
 {
@@ -76,15 +77,43 @@ namespace NSSWeb.Controllers
             {
                 var newExerciseId = (await conn.QueryAsync<int>(sql)).Single();
                 exercise.Id = newExerciseId;
-                var newURL = CreatedAtRoute("GetExercise", new { id = newExerciseId }, exercise);
-                return newURL;
+                return CreatedAtRoute("GetExercise", new { id = newExerciseId }, exercise);
             }
         }
 
         // PUT api/exercises/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id, [FromBody] Exercise exercise)
         {
+            string sql = $@"
+            UPDATE Exercise
+            SET Name = '{exercise.Name}',
+                Language = '{exercise.Language}'
+            WHERE Id = {id}";
+
+            try
+            {
+                using (IDbConnection conn = Connection)
+                {
+                    int rowsAffected = await conn.ExecuteAsync(sql);
+                    if (rowsAffected > 0)
+                    {
+                        return new StatusCodeResult(StatusCodes.Status204NoContent);
+                    }
+                    throw new Exception("No rows affected");
+                }
+            }
+            catch (Exception)
+            {
+                if (!ExerciseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
 
         // DELETE api/exercises/5
@@ -92,5 +121,15 @@ namespace NSSWeb.Controllers
         public void Delete(int id)
         {
         }
+
+        private bool ExerciseExists(int id)
+        {
+            string sql = $"SELECT Id, Name, Language FROM Exercise WHERE Id = {id}";
+            using (IDbConnection conn = Connection)
+            {
+                return conn.Query<Exercise>(sql).Count() > 0;
+            }
+        }
+
     }
 }
